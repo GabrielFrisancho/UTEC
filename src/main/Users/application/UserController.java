481402104d685a50b494b077279f490de70a10ec
GabrielFrisancho <165910476+GabrielFrisancho@users.noreply.github.com>
2024-11-19 14:57:59 -0500

    UserController.java

---
+++ src/main/Users/application/UserController.java
@@ -0,0 +1,77 @@
+package com.api.rest.canvas2.Users.application;
+
+import com.api.rest.canvas2.Grades.dto.GradeResponseDto;
+import com.api.rest.canvas2.Users.domain.UserService;
+import com.api.rest.canvas2.Users.dto.UserRequestDto;
+import com.api.rest.canvas2.Users.dto.UserResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Obtener información del usuario autenticado
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/me")
+    public ResponseEntity<UserResponseDto> getMe() {
+        UserResponseDto userResponse = userService.getMe();
+        return ResponseEntity.ok(userResponse);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
+        UserResponseDto userResponse = userService.getUserById(id);
+        return ResponseEntity.ok(userResponse);
+    }
+
+    @PutMapping("/users/{userId}/role")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<UserResponseDto> updateUserRole(
+            @PathVariable Long userId,
+            @RequestBody Map<String, String> roleRequest) {
+        String newRole = roleRequest.get("role");
+        UserResponseDto updatedUser = userService.updateUserRole(userId, newRole);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_TEACHER')")
+    @GetMapping("/name/{name}")
+    public ResponseEntity<UserResponseDto> getUserByName(@PathVariable String name) {
+        UserResponseDto userResponse = userService.getUserByName(name);
+        return ResponseEntity.ok(userResponse);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping
+    public ResponseEntity<List<UserResponseDto>> getAllUsers() {
+        List<UserResponseDto> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_TEACHER')")
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDto> updateUser(@PathVariable Long id, @RequestBody UserRequestDto userRequestDto) {
+        UserResponseDto updatedUser = userService.updateUser(id, userRequestDto);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+}
